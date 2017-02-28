e588a14ef5cf46f32fc183541f793acb8c5958b0
aslmbc13 <asl_mbc_13@hotmail.com>
2017-02-28 18:21:56 +0000

    Created VS Solution

---
+++ InternetRelayChat/InternetRelayChat/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetRelayChat
+{
+    public class Server
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Just testing");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TDIN-IRC
+
+First assignment of *Technologies of Integration and Distribution*, from the Integrated Master's in Informatics and Computer Engineering of the Faculty of Engineering of the University of Porto.
