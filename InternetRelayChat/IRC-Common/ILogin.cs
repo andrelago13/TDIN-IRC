7c257990e652bdf68a3363dca77971a095b2e9d2
aslmbc13 <asl_mbc_13@hotmail.com>
2017-03-02 15:52:35 +0000

    ILogin

---
+++ InternetRelayChat/IRC-Common/ILogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRC_Common
+{
+    public interface ILogin
+    {
+        bool Login(string username, string password);
+
+        int Register(string username, string realName, string password);
+    }
+}
