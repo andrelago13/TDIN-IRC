6fcecaea5004c84f48ff45613e074fff3be53a18
aslmbc13 <asl_mbc_13@hotmail.com>
2017-03-05 11:47:09 +0000

    Server DB management

--- InternetRelayChat/IRC-Server/Server.cs
+++ InternetRelayChat/IRC-Server/Server.cs
@@ -13,26 +13,61 @@ namespace IRC_Server
     {
         public static void Main(string[] args)
         {
-            // Create database
-            if (!Directory.Exists("res/database.sqlite"))
-            {
-                Directory.CreateDirectory("res");
-                SQLiteConnection.CreateFile("res/database.sqlite");
-
-                //TODO: Create database script
-            }
+            SQLiteConnection conn = InitializeDatabase("res", "database.sqlite");
 
             Console.WriteLine("Server started.");
+            SetupServer();
+            Console.ReadKey();
+            Console.WriteLine("Server ended.");
+        }
+
+        private static SQLiteConnection InitializeDatabase(string dbDir, string dbFile)
+        {
+            string fullPath = dbDir + "/" + dbFile;
+            bool needsInitializing = false;
+
+            // Create database
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(dbDir);
+                SQLiteConnection.CreateFile(fullPath);
+                needsInitializing = true;
+            }
+
+            // Connect to database
+            SQLiteConnection conn = new SQLiteConnection("Data Source=" + fullPath + "; Version=3;");
+            conn.Open();
+
+            if (needsInitializing)
+            {
+                ConfigureDatabase(conn);
+            }
+
+            return conn;
+        }
+
+        private static void ConfigureDatabase(SQLiteConnection conn)
+        {
+            //TODO: Create database script
+
+            string sql = "create table users (nickname varchar(20), realname varchar(40), password varchar(20)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, conn);
+            command.ExecuteNonQuery();
+        }
+        private static void TerminateDatabase(SQLiteConnection conn)
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+
+        private static void SetupServer()
+        {
             // Configure remote objects
             string configFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
             RemotingConfiguration.Configure(configFile, false);
 
-            RemotingConfiguration.RegisterWellKnownServiceType(
-               typeof(Login), "Login",
-               WellKnownObjectMode.Singleton);
-
-            Console.ReadKey();
-            Console.WriteLine("Server ended.");
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Login), "Login", WellKnownObjectMode.Singleton);
         }
     }
 }
